f0d4c6acb3c91f2cfa058bd3ce5bf0834d6eea83
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-02-07 20:42:12 -0300

    Hierarquia de componentes

--- fundamentos/src/App.js
+++ fundamentos/src/App.js
@@ -1,6 +1,7 @@
 // Componentes
 import FirstComponent from './components/FirstComponent';
 import TemplateExpression from './components/TemplateExpression';
+import MyComponent from './components/MyComponent';
 
 // Styles / css
 import './App.css';
@@ -11,6 +12,7 @@ function App() {
       <h1>Fundamentos</h1>
       <FirstComponent />
       <TemplateExpression />
+      <MyComponent />
     </div>
   );
 }
--- fundamentos/src/components/FirstComponent.js
+++ fundamentos/src/components/FirstComponent.js
@@ -1,8 +1,13 @@
 import React from 'react'
 
+import MyComponent from './MyComponent'
+
 const FirstComponent = () => {
   return (
-    <h3>Meu primeiro componente</h3>
+    <div>
+      <h3>Meu primeiro componente</h3>
+      <MyComponent />
+    </div>
   )
 }
 
