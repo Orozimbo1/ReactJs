146a9c09299011650e883a5b5589440884a5fe8c
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-02-08 20:26:21 -0300

    Previous state

--- avancando-no-react/src/components/ListRender.js
+++ avancando-no-react/src/components/ListRender.js
@@ -1,7 +1,25 @@
 import { useState } from 'react'
 
 const ListRender = () => {
-  const [list] = useState(["Matheus", "Marcus", "Rafael"])
+  const [list] = useState(["Matheus", "Marcus", "Rafael", "Carlos", "Andre"])
+
+  const [users, setUsers] = useState([
+    { id: 0, age: 22, name: "Andre" },
+    { id: 1, age: 22, name: "Matheus" },
+    { id: 2, age: 22, name: "Rafael" },
+    { id: 3, age: 22, name: "Marcus" }
+  ])
+  
+  const handleDelete = () => {
+    const randomNumber = Math.floor(Math.random() * 5)
+
+    setUsers((prevUsers) => {
+      console.log(prevUsers)
+
+      return prevUsers.filter((user) => randomNumber !== user.id)
+      
+    })
+  }
 
   return (
     <div>
@@ -12,6 +30,14 @@ const ListRender = () => {
           </li>
         ))}
       </ul>
+      <ul>
+        {users.map((user, index) => (
+          <li key={user.id}>
+            {user.name} - {user.age}
+          </li>
+        ))}
+      </ul>
+      <button onClick={handleDelete}>Delete random user</button>
     </div>
   )
 }
