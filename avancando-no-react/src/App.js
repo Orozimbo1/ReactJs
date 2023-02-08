d4e17c33db6b37c63c8c408e2ae8e37cd8457d33
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-02-08 19:42:56 -0300

    imagens no repositório public

---
+++ avancando-no-react/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Avançando em react</h1>
+      {/*Imagem em public*/}
+      <div>
+        <img src="/img1.jpg" alt="Paisagem" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
