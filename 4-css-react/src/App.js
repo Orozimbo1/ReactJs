4524c0df1426a2553b53a083377229447d428319
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-02-09 20:37:11 -0300

    CSS inline dinâmico

--- 4-css-react/src/App.js
+++ 4-css-react/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import MyComponent from './components/MyComponent';
 
 function App() {
+  const n = 11
+
   return (
     <>
       {/*CSS Global*/}
@@ -14,6 +16,8 @@ function App() {
       <p style={{ backgroundColor: "orange", color: "gold", padding: "25px", borderTop: "1px solid black" }}>
         Este parágrafo foi estilizado de forma inline
       </p>
+      {/*CSS inline dinâmico*/}
+      <h2 style={n < 10 ? ({ color: "purple", backgroundColor: "brown" }) : ({ color: "gray", backgroundColor: "violet" })}>CSS dinâmico</h2>
     </>
   );
 }
