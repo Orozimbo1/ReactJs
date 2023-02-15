2a5739cf72882925e382bf714aa63366fac51493
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-02-15 20:12:35 -0300

    Adicionando Dados na fake Api com React

--- 6-http-react/src/App.jsx
+++ 6-http-react/src/App.jsx
@@ -7,7 +7,10 @@ const url = 'http://localhost:3000/products'
 function App() {
   const [products, setProducts] = useState([])
 
-  // resgatando dados
+  const [name, setName] = useState('')
+  const [price, setPrice] = useState('')
+
+  // 1 - resgatando dados
   useEffect(() => {
     const fetchData = async () => {
       const res = await fetch(url)
@@ -17,6 +20,24 @@ function App() {
 
     fetchData()
   }, [])
+
+  // 2 - Adicionando produtos
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const product = {
+      name,
+      price
+    }
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(product)
+    })
+  }
 
   return (
     <div className="App">
@@ -28,6 +49,20 @@ function App() {
         </li>  
       ))}
       </ul>
+      <div className="add-products">
+        <h2>Cadastrar Produto</h2>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input type="text" value={name} name='name' onChange={(e) => setName(e.target.value)} />
+          </label>
+          <label>
+            Preço:
+            <input type="number" value={price} name='price' onChange={(e) => setPrice(e.target.value)} />
+          </label>
+          <input type="submit" value="Cadastrar" />
+        </form>
+      </div>
     </div>
   )
 }
