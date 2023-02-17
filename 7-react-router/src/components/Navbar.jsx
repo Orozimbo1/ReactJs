b58c7ce29f02e4792e567d6a3635ad256c4367f2
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-02-16 21:38:48 -0300

    Link ativo

--- 7-react-router/src/components/Navbar.jsx
+++ 7-react-router/src/components/Navbar.jsx
@@ -1,13 +1,20 @@
 import './Navbar.css'
 
 // 2 - Link com react router
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <nav>
-      <Link to='/'>Home</Link>
-      <Link to='/about'>Sobre</Link>
+      {/* <Link to='/'>Home</Link>
+      <Link to='/about'>Sobre</Link> */}
+      <NavLink 
+      to='/' 
+      // className={({isActive}) => (isActive ? 'esta-ativo' : 'nao-ativo')} Jeito antigo de deixar o link ativo quando renderizado
+      >
+        Home
+      </NavLink>
+      <NavLink to='/about'>Sobre</NavLink>
     </nav>
   )
 }
