24f0ce006d8f5ff9bb55bf0f67820125efadd037
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-02-17 20:47:40 -0300

    Criando o provider

--- 8-context-api-react/src/context/index.js
+++
@@ -1,2 +0,0 @@
-export {default as CounterContext} from './CounterContext'
-
--- 8-context-api-react/src/pages/Home.jsx
+++ 8-context-api-react/src/pages/Home.jsx
@@ -1,7 +1,14 @@
+import { useContext } from "react"
+
+import { CounterContext } from "../context/CounterContext"
+
 const Home = () => {
+  const { counter } = useContext(CounterContext)
+
   return (
     <div>
       <h1>Home</h1>
+      <h2>O valor do contador é: {counter}</h2>
     </div>
   )
 }
