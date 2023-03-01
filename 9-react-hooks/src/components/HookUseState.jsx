d2761f0dd35c79dda7e88391ca360943e5b7a33e
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-02-28 22:26:24 -0300

    useState e inputs

--- 9-react-hooks/src/components/HookUseState.jsx
+++ 9-react-hooks/src/components/HookUseState.jsx
@@ -9,23 +9,43 @@ const HookUseState = () => {
   const changeNames = () => {
 
     userName = 'Joao Souza'
-
     setName('Matheus Orozimbo')
 
-    console.log(userName)
-    
+    // console.log(userName)
   }
-  console.log(name)
+  // console.log(name)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    // Enviando a uma API
+    console.log(age)
+  }
   
+  // 2- useState e inputs
+  const [age, setAge] = useState(18)
 
   return (
     <div>
-      {/* 1 - useState */}
+    {/* 1 - useState */}
     <h2>useState</h2>
     <p>Variável: {userName}</p>
     <p>useState: {name}</p>
     <button onClick={changeNames}>Mudar nomes</button>
 
+    {/* 2 - useState e input */}
+    <p>Digite a sua idade:</p>
+    <form onSubmit={handleSubmit}>
+      <label>
+        <input 
+          type="text" 
+          value={age} 
+          onChange={(e) => setAge(e.target.value)}
+        />
+      </label>
+      <input type="submit" value="Enviar" />
+    </form>
+    <p>Você tem {age} anos.</p>
     <hr />
     </div>
   )
