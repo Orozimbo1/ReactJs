222272b9b4de2b68f2b06f1970ca90543e59d3e7
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-03-20 20:25:47 -0300

    Arrow functions

--- 10-javascript-moderno/index.js
+++ 10-javascript-moderno/index.js
@@ -36,3 +36,48 @@ console.log(c) // 10
 // c = 7 // ERROR, constantes não são mutáveis
 
 // 2- Arrow function
+const sum = function(a, b) {
+  return a + b
+}
+
+// const arrowSum = (a, b) => {
+//   return a + b
+// }
+const arrowSum = (a, b) => a + b // com parametros sem corpo
+
+console.log(sum(5,9))
+console.log(arrowSum(3,9))
+
+const greeting = (name) => { // com parametros e com corpo
+  if(name) {
+    return 'Olá ' + name + '!'
+  } else {
+    return 'Olá'
+  }
+}
+
+console.log(greeting('Matheus'))
+console.log(greeting())
+
+const test = () => console.log('testou') // sem parametros e sem corpo
+
+test()
+
+const user = {
+  name: 'Matheus',
+  sayUserName() {
+    var self = this
+    setTimeout(function() {
+      console.log(self)
+      console.log(`UserName: ${self.name}`)
+    }, 500)
+  },
+  sayUserNameArrow() {
+    setTimeout(() => {
+      console.log(this)
+      console.log(`UserName ${this.name}`)
+    }, 500)
+  }
+}
+user.sayUserName()
+user.sayUserNameArrow()
