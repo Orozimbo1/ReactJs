5d94d7276c34906ee0d35537fdeadeb00b903897
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-03-21 20:02:51 -0300

    Status na resposta

--- 11-API/index.js
+++ 11-API/index.js
@@ -15,13 +15,22 @@ app.use(express.json())
 
 // rotas
 app.get('/', (req, res) => {
-  res.json({message: 'Primeira rota criada com sucesso!'})
+  // 3- Adicionando status nas respostas
+  res.status(200).json({message: 'Primeira rota criada com sucesso!'})
 })
 // 2- Criando a rota de post
 app.post('/createproduct', (req, res) => {
   const { name, price } = req.body
 
-  return res.json({produto: {name, price}})
+  // 3- Adicionando status nas respostas
+  if(!name) {
+    return res.status(422).json({message: 'O campo nome é obrigatório.'})
+  }
+  if(!price) {
+    return res.status(422).json({message: 'O campo nome é obrigatório.'})
+  }
+  // 3- Adicionando status nas respostas
+  return res.status(201).json({produto: {name, price}})
 })
 
 app.listen(port, () => {
