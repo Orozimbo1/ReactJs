935bb8078e6b7e5fe5fcf0d3d4cb1fcb6a14907c
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-03-21 21:15:05 -0300

    Criando o primeiro componente

---
+++ 13-react-typescript/react-tsx/src/components/FirstComponent.tsx
@@ -0,0 +1,10 @@
+// 3- Componente
+import { ReactElement } from "react"
+
+function FirstComponent(): ReactElement {
+  return (
+    <div>Meu primeiro componente</div>
+  )
+}
+
+export default FirstComponent
