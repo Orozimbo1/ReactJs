c2fdfa26836b0830b948d8e48261b3ef0e08d931
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-03-21 21:03:11 -0300

    Funções no componente

--- 13-react-typescript/react-tsx/src/App.tsx
+++ 13-react-typescript/react-tsx/src/App.tsx
@@ -1,15 +1,22 @@
 import './App.css'
 
 function App() {
+
   // 1- Variáveis
   const name: string = 'Matheus'
   const age: number = 24
   const isWorking: boolean = true
 
+  // 2- Funções
+  const userGreeting = (name: string): string => {
+    return `Olá ${name}`
+  }
+
   return (
     <div className="App">
       <h1>Typescript com React</h1>
       <h2>Nome: {name}, {age} anos. Trabalhando: {isWorking && 'Sim'}</h2>
+      <h3>{userGreeting(name)}</h3>
     </div> 
   )
 }
